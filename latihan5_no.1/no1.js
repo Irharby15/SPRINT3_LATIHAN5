93d9f6c93394548e207b5c859c500ca69d9400f7
manshir65 <albarragans9786@gmail.com>
2021-08-02 14:08:18 +0700

    latihan5

---
+++ latihan5_no.1/no1.js
@@ -0,0 +1,10 @@
+function kalikan(hasilkali) {
+
+    console.log(num1 * num2)
+
+};
+var num1 = 12;
+var num2 = 4;
+
+var hasilkali = kalikan(num1, num2);
+console.log(hasilkali)
---
+++ latihan5_no.2/no2.js
@@ -0,0 +1,12 @@
+for (var y = 1; y <= 20; y++) {
+    if (y % 2 == 0) {
+        console.log(y + ".Berkualitas")
+    } else if (y % 2 && y % 3 == 0) {
+        console.log(y + ".Ilove conding")
+    } else if (y != 0) {
+        console.log(y + ".Santai")
+    } else {
+        console,
+        log(y)
+    }
+}
